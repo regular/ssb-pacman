70fe81055987df0f1f2d9dbc71e1a0b3c5256ced
Jan Bölsche <regular.gonzales@googlemail.com>
2018-07-25 11:12:35 +0200

    Automaticcaly compare master keys contained in package "archlinux-keyring" to those listed on the website and the git repository

--- signing.sh
+++ signing.sh
@@ -11,6 +11,10 @@ tmpdir=$(mktemp -d)
 wget -P $tmpdir $keyring_pkg_url{,.sig}
 archive=$(ls $tmpdir --hide=*.sig)
 bsdtar -C $tmpdir -xvJf $tmpdir/$archive
+echo
+
+git clone https://git.archlinux.org/archlinux-keyring.git $tmpdir/git-checkout
+echo
 
 keyringfile=$tmpdir/usr/share/pacman/keyrings/archlinux.gpg
 $gpg --import $keyringfile
@@ -29,19 +33,31 @@ while read -ra fields; do
     nopk=1
   fi
 done < <($gpg --verify $tmpdir/*.sig $tmpdir/*.xz 2>&1)
-  
-if [[ nopk -gt 0 && -n pkey ]]; then
-  echo "Downloading key: $pkey"
-  $gpg --receive-key ${pkey: -16}
-fi
+ 
+# It would be alarming, if the signing key is not contained in the package's keyring,
+#if [[ nopk -gt 0 && -n pkey ]]; then
+#  echo "Downloading key: $pkey"
+#  $gpg --receive-key ${pkey: -16}
+#fi
 
 $gpg --list-sigs ${pkey: -16}
 
+cat $tmpdir/usr/share/pacman/keyrings/archlinux-trusted | sort | cut -d":" -f1 > $tmpdir/packaged_keys
 echo "Master keys listed in archlinux-keyring package:"
-
-rm pipe1 || true
-mkfifo pipe1
-(cat $tmpdir/usr/share/pacman/keyrings/archlinux-trusted | cut -d":" -f1 | sort | xargs -I% $gpg --with-colons --list-key %) | node tools/parse-gpg.js
+(cat $tmpdir/packaged_keys | xargs -I% $gpg --with-colons --list-key %) | node tools/parse-gpg.js
 echo
-echo "Master keys listed on website:"
-node node_modules/get-arch-master-keys/index.js | sort
+
+cat $tmpdir/git-checkout/archlinux-trusted | sort | cut -d":" -f1 > $tmpdir/keys-from-git
+echo "Master keys according to git.archlinux.org/archlinux-keyring.git:"
+(cat $tmpdir/keys-from-git | xargs -I% $gpg --with-colons --list-key %) | node tools/parse-gpg.js
+echo
+
+node_modules/.bin/get-arch-master-keys | sort | cut -d"|" -f1 | tr -d ' ' > $tmpdir/keys-from-website
+echo "Master keys listed on official website:"
+node_modules/.bin/get-arch-master-keys | sort 
+echo
+
+pushd $tmpdir > /dev/null
+diff -ws --to-file packaged_keys keys-from-git keys-from-website
+popd > /dev/null
+
