f7c8ebe27895c5eb1c8105e0669d70baf8e05183
Jan Bölsche <regular.gonzales@googlemail.com>
2018-08-03 16:48:16 +0200

    Move unused files into .unused

--- ssb-pacman-shrinkwrap.sh
+++
@@ -1,4 +0,0 @@
-set -eu -o pipefail
-
-for p in $(pacman -Qe|tr " " "-"); do printf "%s explicit %s\n" $(cat /var/lib/pacman/local/$p/ssb-key) $p; done
-for p in $(pacman -Qd|tr " " "-"); do printf "%s dependency %s\n" $(cat /var/lib/pacman/local/$p/ssb-key) $p; done
