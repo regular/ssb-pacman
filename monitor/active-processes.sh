b5174018b571762f6ff4ca5f86cb6a9685d8e961
Jan Bölsche <regular.gonzales@googlemail.com>
2018-08-08 16:24:20 +0200

    Generate en_US.UTF8 locale as part of bootstrap. Fix montor and pacman-sync scripts

--- monitor/active-processes.sh
+++ monitor/active-processes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 set -eu -o pipefail
-parentpid=$(pgrep update-all)
-echo "update-all parent pid: $parentpid"
-ps -o "pid,command" --ppid $parentpid | grep update.sh | awk '{ print $1,$4,$5 }'
+parentpid=$(pgrep -x ssb-pacman-sync|head -n1)
+echo "sync parent pid: $parentpid"
+ps -o "pid,command" --ppid "$parentpid" | grep ssb-pacman-sync-repo #| awk '{ print $1,$4,$5 }'
 
