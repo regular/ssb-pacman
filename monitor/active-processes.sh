7bcc1a6bbc724d0d7b6e9b7cd2c5c7a441191ee1
Jan Bölsche <regular.gonzales@googlemail.com>
2018-08-12 10:57:38 +0200

    Fix monitor/active-processes

--- monitor/active-processes.sh
+++ monitor/active-processes.sh
@@ -2,5 +2,5 @@
 set -eu -o pipefail
 parentpid=$(pgrep -x ssb-pacman-sync|head -n1)
 echo "sync parent pid: $parentpid"
-ps -o "pid,command" --ppid "$parentpid" | grep ssb-pacman-sync-repo #| awk '{ print $1,$4,$5 }'
+ps -wwo "pid,command" --ppid "$parentpid" | grep repo | awk '{ print $1,$4,$5 }'
 
